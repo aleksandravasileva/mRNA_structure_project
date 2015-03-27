6c8cf69e69104d2fb473eb13cb8dbcddcee3a088
Aleksandra Vasileva <daucussativus7@gmail.com>
2015-03-27 20:24:05 +0300

    3_try_to_run_RNAfold

--- Subprocess_try.py
+++ Subprocess_try.py
@@ -13,16 +13,19 @@ rnafold_proc = sp.Popen("RNAfold < {}".format(path), shell=True, stdout=sp.PIPE)
 rnafold_proc.wait()
 rnafold_result = rnafold_proc.communicate()[0].decode().splitlines()
 
-seq_name = rnafold_result[0].split(">")[1]
+seq_name = rnafold_result[0][1:]
 
 draft_folding_string = rnafold_result[2].split(" ")[0]
 
 try:
-    os.mkdir("{}".format(out_path))
+    os.mkdir(out_path)
 except FileExistsError:
-    print("Don't create directory next time")
-finally:
-    with open('{}/{}_RNAfold_output.fasta'.format(out_path, seq_name),
-              'w') as rnafold_new_file:
-        rnafold_new_file.write(draft_folding_string)
+    print("Don't create directory next time", file=sys.stderr)
+    # Я все равно, правда, не понимаю, что происходит в этой команде
+    # Я потом могу отдельный файл с ошибками вывести или зачем это вообще?
+
+path_to_rnafold_file = '{}/{}_RNAfold_output.fasta'.format(out_path, seq_name)
+
+with open(path_to_rnafold_file, 'w') as rnafold_file:
+    rnafold_file.write(draft_folding_string)
 
